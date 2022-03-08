b2e1828ecd13bb545af71ad0635e38bfa71d3e20
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-08 15:54:25 -0800

    feat: Use environ to load some settings

--- mysite_project/mysite_config/settings.py
+++ mysite_project/mysite_config/settings.py
@@ -12,18 +12,29 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 
+import environ
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+##############################
+# SETUP ENVIRONMENT VARIABLES
+##############################
+# Set casting, default value
+env = environ.Env(
+    DEBUG=(bool, False)
+)
+# Take environment variables from .env file
+environ.Env.read_env(BASE_DIR / '.env')
+##############################
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nwl+mg$c@ps)(v@)m#sd%1opzo2eac+#bq@#%r)6pbk++t%wp4'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
@@ -74,13 +85,7 @@ WSGI_APPLICATION = 'mysite_config.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
-        'HOST': ''
-    }
+    'default': env.db(),
 }
 
 
