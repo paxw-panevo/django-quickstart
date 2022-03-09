008c958202a62045cacc871930116e7e00729fa0
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 07:42:35 -0800

    feat: Create views for Question model

--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello, world! You're at the polls index.")
+
+def question_detail(request, question_id):
+    return HttpResponse(f"You're looking at question {question_id}.")
+
+def question_results(request, question_id):
+    return HttpResponse(f"You're looking at results of question {question_id}.")
+
+def question_vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
