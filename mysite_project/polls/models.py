0b1f6e38060972ce95070e15c93dd74944dd371f
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-08 19:08:44 -0800

    feat: Determine whether Q was published recently

--- mysite_project/polls/models.py
+++ mysite_project/polls/models.py
@@ -1,4 +1,7 @@
+from datetime import timedelta
+
 from django.db import models
+from django.utils import timezone
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -6,6 +9,10 @@ class Question(models.Model):
 
     def __str__(self):
         return f"({self.pk}) {self.question_text}" 
+    
+    def was_published_recently(self) -> bool:
+        the_day_before = timezone.now() - timedelta(days=1)
+        return self.pub_date >= the_day_before
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
