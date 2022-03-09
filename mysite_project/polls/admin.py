7746633e99cc907b2f0e06806705c89afcffd0fd
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 12:35:01 -0800

    feat: Allow creation of Choice(s) w/in Question forms

--- mysite_project/polls/admin.py
+++ mysite_project/polls/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Choice, Question
+
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+
+    # This means to provide 3 "Choice" fields by default (i.e. creating)
+    extra = 3
+
 
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {'fields': ['question_text']}),
         ('Date Information', {'fields': ['pub_date']}),
     ]
+    inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
