89a3ae953382422241e861eba5c1c89214cba4d3
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 08:47:38 -0800

    refactor: Namespace polls' urls

--- mysite_project/polls/urls.py
+++ mysite_project/polls/urls.py
@@ -2,6 +2,10 @@ from django.urls import path
 
 from . import views
 
+# This is used for namespacing purposes.
+# https://docs.djangoproject.com/en/3.2/intro/tutorial03/#namespacing-url-names
+app_name = 'polls'
+
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:question_id>/', views.question_detail, name="detail"),
