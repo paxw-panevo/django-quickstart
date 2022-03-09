8086d56ef904eaefcb40ca36de722552e67fecb5
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 08:12:47 -0800

    feat: List last 5 polls

--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, world! You're at the polls index.")
+    latest_questions = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_questions': latest_questions}
+    return HttpResponse(template.render(context, request))
 
 def question_detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
