330fb81e6c1aabaf1834fdbb3f18da0e7828fdba
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 08:33:07 -0800

    feat: Properly implement Question detail page
    - Load q from database; Raise 404 if q doesn't exist
    - Use templates

--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
 
 from .models import Question
@@ -10,7 +10,15 @@ def index(request):
     return render(request, 'polls/index.html', context)
 
 def question_detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist as exc:
+        # Re: `raise ... from` see,
+        # https://stackoverflow.com/questions/24752395/python-raise-from-usage
+        # https://www.python.org/dev/peps/pep-3134/
+        raise Http404('Question does not exist.') from exc
+    context = {'question': question}
+    return render(request, 'polls/detail.html', context)
 
 def question_results(request, question_id):
     return HttpResponse(f"You're looking at results of question {question_id}.")
