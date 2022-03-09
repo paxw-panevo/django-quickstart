0206bcf8d5bd90ebc9e486532689792a25f22130
Pax <100712424+paxw-panevo@users.noreply.github.com>
2022-03-09 08:14:28 -0800

    refactor: Use render function

--- mysite_project/polls/views.py
+++ mysite_project/polls/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 
 from .models import Question
 
 
 def index(request):
     latest_questions = Question.objects.order_by('-pub_date')[:5]
-    template = loader.get_template('polls/index.html')
     context = {'latest_questions': latest_questions}
-    return HttpResponse(template.render(context, request))
+    return render(request, 'polls/index.html', context)
 
 def question_detail(request, question_id):
     return HttpResponse(f"You're looking at question {question_id}.")
